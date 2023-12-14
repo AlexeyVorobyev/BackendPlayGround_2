092e4271cfb5714f0c2fad3220db04138f07d4a4
miste <mister.alex49@yandex.ru>
2023-12-14 11:04:09 +0300

    repository.py, singleton and add_repr decorators

---
+++ src/SQLAlchemy/orm.py
@@ -0,0 +1,16 @@
+from src.SQLAlchemy.base import Base
+from src.SQLAlchemy.connection import PostgreAlchemyConnection
+from src.utils.classes.config import Config
+from src.utils.decorators.singleton import singleton
+
+
+@singleton
+class PostgreAlchemyORM:
+    def __init__(self):
+        self._session_maker = PostgreAlchemyConnection(Config()).session_maker
+        self._engine = PostgreAlchemyConnection(Config()).engine
+
+    def create_database(self):
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+
